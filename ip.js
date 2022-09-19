ec55a63a7ef239c42546c178f772fc786b9bcb3e
Albert <albertzeap@gmail.com>
2022-09-18 22:51:38 -0700

    Implemented ip search and rendered results

--- ip.js
+++ ip.js
@@ -1,3 +1,4 @@
+// Call the API that returns the users IP address
 let callIP = async () => {
 
     try{
@@ -18,8 +19,31 @@ let callIP = async () => {
 
 }
 
-let userIP = document.getElementById("user-ip");
+// Function that calls the API for searching for the details of an IP address
+let fetchSearchIP = async (requestedIP) => {
+    try{
+        let response = await fetch(`https://ipinfo.io/${requestedIP}?token=16b9f720767f17`);
 
+        if(!response){
+            let message =  `Error: ${response.status} ${response.statusText}`;
+            let error = new Error(message);
+            throw error;
+        }
+
+        const searchedIp = await response.json();
+        return searchedIp;
+
+    } catch(err){
+        console.error(err);
+    }
+}
+
+// Variable for holding the element in which the user IP will be displayed
+let userIP = document.getElementById("user-ip");
+let ipContent = document.getElementById("ip-content");
+let ipContainer = document.getElementById("ip-container");
+
+// Renders the users IP onto the screen
 let renderUserIP = () => {
     callIP().then((ipObject) => {
         console.log(ipObject);
@@ -27,4 +51,49 @@ let renderUserIP = () => {
     });
 }
 
+
+let rendersearchIP = (requestedIP) => {
+    fetchSearchIP(requestedIP).then((ipObject) => {
+        let ipDetails = document.getElementById("details-container");
+        ipDetails.innerHTML = ` 
+        <h3>Details</h3>
+        <p>IP: ${ipObject.ip}
+        <p>Organization: ${ipObject.org}
+        <p>Hostname: ${ipObject.hostname}
+        <p>City: ${ipObject.city}</p>
+        <p>State: ${ipObject.region}</p>
+        <p>ZIP: ${ipObject.postal} </p>
+        <p>Country: ${ipObject.country}</p>
+        `;
+        console.log(ipObject);
+    });
+}
+
+// Hides the details div until actual search is initiated
+let hideDiv = () => {
+    let detailsDiv = document.getElementById("details-container");
+    detailsDiv.style.display = "none";
+}
+
+// Shows the details div when search is initiated and hides disclaimer message
+let showDiv = () => {
+    let detailsDiv = document.getElementById("details-container");
+    let disclaimer = document.getElementById("disclaimer");
+    detailsDiv.style.display = "block";
+    disclaimer.style.display = "none";
+}
+
+// Hide details div
+hideDiv();
+
+// Always automatically render the users IP onto the page
 renderUserIP();
+
+// IP information about requested IP address is only rendered if user desires to
+document.querySelector("form").addEventListener("submit",(e) => {
+    e.preventDefault();
+    let requestedIP = document.getElementById("ip-search").value;
+    console.log(requestedIP);
+    rendersearchIP(requestedIP);
+    showDiv();
+});
